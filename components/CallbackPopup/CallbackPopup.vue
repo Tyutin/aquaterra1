5b76d2c2f3698564b49059aee65169e2677042bb
Tyutin_Kirill <kerya.tyutin@yandex.ru>
2022-06-08 02:13:46 +0400

    callback form validation

--- components/CallbackPopup/CallbackPopup.vue
+++ components/CallbackPopup/CallbackPopup.vue
@@ -1,11 +1,41 @@
 <template>
-    <form class="callback-popup" @click.stop v-if="callbackPopupIsOpen">
+    <form class="callback-popup" @click.stop v-if="callbackPopupIsOpen" @submit.prevent="submitForm">
         <button class="callback-popup__close" @click.prevent="closeCallbackPopup"/>
         <p class="callback-popup__header">Оставьте заявку</p>
         <p class="callback-popup__subheader">Чтобы получить консультацию или сделать заказ</p>
-        <input type="text" name="name" placeholder="Ваше имя*" class="callback-popup__input">
-        <input type="text" name="phone" placeholder="Ваш телефон*" class="callback-popup__input">
-        <textarea name="description" cols="30" rows="3" placeholder="Описание объекта" class="callback-popup__textarea"></textarea>
+        <label class="callback-popup__label" :class="{'callback-popup__label_error': errors.name}">
+            <input 
+                type="text"
+                name="name"
+                v-model="name"
+                placeholder="Ваше имя*"
+                class="callback-popup__input"
+                @input="nameHandler"
+                :class="{'callback-popup__input_error': errors.name}"
+            >
+            <span class="callback-popup__error-message" v-if="errors.name">Введите корректное имя</span>
+        </label>
+        <label class="callback-popup__label" :class="{'callback-popup__label_error': errors.phone}">
+            <input 
+                type="text" 
+                name="phone" 
+                v-model="phone" 
+                placeholder="Ваш телефон*" 
+                class="callback-popup__input" 
+                v-mask="'+7(###)-###-##-##'"
+                @input="phoneHandler"
+                :class="{'callback-popup__input_error': errors.phone}"
+            >
+            <span class="callback-popup__error-message" v-if="errors.phone">Введите корректый телефон</span>
+        </label>
+        <textarea 
+            name="description" 
+            v-model="description" 
+            cols="30" 
+            rows="3" 
+            placeholder="Описание объекта" 
+            class="callback-popup__textarea"
+        />
         <input type="submit" value="Получить консультацию" class="callback-popup__submit">
         <p class="callback-popup__agreement">Оставляя заявку, Вы соглашаетесь на обработку персональных данных. Ваши данные ни в коем случае не будут переданы третьим лицам.</p>
     </form>
@@ -13,6 +43,17 @@
 
 <script>
     export default {
+        data() {
+            return {
+                name: '',
+                phone: '',
+                description: '',
+                errors: {
+                    name: false,
+                    phone: false
+                }
+            }
+        },
         computed: {
             callbackPopupIsOpen() {
             return this.$store.state.popups.callbackPopupIsOpen 
@@ -21,6 +62,24 @@
         methods: {
             closeCallbackPopup() {
                 this.$store.commit('popups/closeCallbackPopup')
+            },
+            nameHandler() {
+                this.errors.name = false
+            },
+            phoneHandler() {
+                this.errors.phone = false
+                if(!(this.phone[3] === '9' || this.phone[3] === '3')) {
+                    this.phone = this.phone.slice(0, 3) + this.phone.slice(4, this.phone.length)
+                }
+            },
+            submitForm() {
+                this.name = this.name.replace(new RegExp("\\ +", "gm"), ' ').trim()
+                if(this.name.length < 2) {
+                    this.errors.name = true
+                }
+                if(this.phone.length < 16) {
+                    this.errors.phone = true
+                }
             }
         },
     }
@@ -29,8 +88,8 @@
 <style lang="scss" scoped>
 .callback-popup {
     position: relative;
-    width: 400px;
-    padding: 30px;
+    width: 424px;
+    padding: 30px 42px;
     display: flex;
     flex-direction: column;
     background-color: #fff;
@@ -79,12 +138,35 @@
             font-size: 16px;
         }
     }
+    
+    &__label {
+        position: relative;
+        margin-bottom: 15px;
+
+        &_error {
+            padding-bottom: 15px;
+        }
+    }
+
+    &__error-message{
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        font-size: 12px;
+        color: red;
+    }
 
     &__input {
+        width: 100%;
         height: 40px;
-        margin-bottom: 15px;
         padding: 2px 3px;
         font-size: 20px;
+        border: 1px solid gray;
+        border-radius: 4px;
+
+        &_error {
+            border-color: red;
+        }
     }
 
     &__textarea {
